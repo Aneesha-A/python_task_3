4b5c05eb95f4c53e9172df907d6855e364eee76e
Aneesha-A <aneeshaa680@gmail.com>
2025-08-17 19:39:23 +0530

    Add files via upload

---
+++ math_module.py
@@ -0,0 +1,8 @@
+import math
+n=int(input("Enter the number: "))
+square_root=math.sqrt(n)
+logarithm=math.log(n)
+sine=math.sin(n)
+print("Square root:",square_root)
+print("Logarithm:",logarithm)
+print("Sine:",sine)
